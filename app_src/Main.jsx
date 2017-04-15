da8a82e8df2bcb12edc22e92f47b742fb25e28ff
RyosukeCla <ryosukeclarinet@gmail.com>
2017-04-16 03:40:17 +0900

    add materialui env

--- app_src/Main.jsx
+++ app_src/Main.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import App from './component/App.jsx';
+
 class Main extends React.Component {
+  muiTheme() {
+    return getMuiTheme(
+      {}
+    );
+  }
+
   render() {
     return(
-      <div>Main</div>
+      <MuiThemeProvider muiTheme={this.muiTheme()}>
+        <App />
+      </MuiThemeProvider>
     );
   }
 }
 
 window.onload = function() {
+  injectTapEventPlugin();
   ReactDOM.render(
   <Main />
   ,
