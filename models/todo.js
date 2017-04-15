b876b205d98d8345a4824052f0004bf2b74cd722
RyosukeCla <ryosukeclarinet@gmail.com>
2017-04-16 03:37:13 +0900

    add task

--- models/todo.js
+++ models/todo.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
+  task_id: {type: String, required: true},
   title: {type: String, required: true},
-  content: {type: String, required: false},
   has_done: {type: Boolean, default: false},
   created: {type: Date, default: Date.now},
   updated: {type: Date, default: Date.now}
